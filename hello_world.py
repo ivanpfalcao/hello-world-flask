2aac68539f7f52094ecf25307df77e3665a35fca
Ivan <ivanpfalcao@hotmail.com>
2022-08-05 15:12:51 -0300

    Initial commit

---
+++ hello_world.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify, request
+  
+app = Flask(__name__)
+  
+  
+@app.route('/hello_world', methods=['GET'])
+def helloworld():
+    if(request.method == 'GET'):
+        data = {"message": "Hello World"}
+        return jsonify(data)
+  
+  
+if __name__ == '__main__':
+    app.run(debug=True)
